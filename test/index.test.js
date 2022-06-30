4acca2d14f0c8aa23f95ef79b6e748ef7985c639
Ilso Christ Gouvêa Barbosa <ilso_1000@hotmail.com>
2022-06-29 21:20:33 -0300

    test: add index tests.js

---
+++ test/index.test.js
@@ -0,0 +1,18 @@
+const pegaArquivo = require('../index');
+
+const arrayResult = [{
+    FileList: 'https://developer.mozilla.org/pt-BR/docs/Web/API/FileList'
+}]
+describe('pegaArquivo::', () => {
+    it('deve ser uma função', () => {
+        expect(typeof pegaArquivo).toBe('function');
+    })
+    it('deve retornar array com resultados', async() => {
+        const resultado = await pegaArquivo('C:/Users/ilso_/OneDrive/Área de Trabalho/LibMarkdown/test/arquivos/texto1.md')
+        expect(resultado).toEqual(arrayResult)
+    })
+    it('deve retornar mensagem "não há links"', async() => {
+        const resultado = await pegaArquivo('C:/Users/ilso_/OneDrive/Área de Trabalho/LibMarkdown/test/arquivos/texto1_semLinks.md')
+        expect(resultado).toBe('não há links');
+    })
+})
